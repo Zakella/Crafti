3e489011563a822adc95ba59df35764ea017786d
zapolski.slava@gmail.com <philips99>
2022-05-11 22:32:47 +0300

    1,2

--- redact_images.py
+++ redact_images.py
@@ -9,20 +9,24 @@ dir_out = os.getcwd() + "\\images_out\\"
 files = os.listdir(dir_in)
 
 for im in files:
-    print("Start of image processing..." + im)
-    img = Image.open(dir_in + im)
-    img = img.convert("RGB")
-    datas = img.getdata()
-    new_image_data = []
-    for item in datas:
-        if item[0] in list(range(190, 256)):
-            new_image_data.append((255, 255, 255))
-        else:
-            new_image_data.append(item)
+    try:
+        print("Start of image processing..." + im)
+        img = Image.open(dir_in + im)
+        img = img.convert("RGB")
+        datas = img.getdata()
+        new_image_data = []
+        for item in datas:
+            if item[0] in list(range(190, 256)):
+                new_image_data.append((255, 255, 255))
+            else:
+                new_image_data.append(item)
 
-    img.putdata(new_image_data)
-    img.save(dir_out + "out_" + im)
-    print("End ..." + im)
+        img.putdata(new_image_data)
+        img.save(dir_out + "out_" + im)
+        print("End ..." + im)
+    except:
+        print("fail" + im)
+
     # img.show()
 
 
